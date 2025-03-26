9529b5c9f28b6b7ad6e484f4fd64c00d981b6833
JowiRey <95541670+JowiRey@users.noreply.github.com>
2025-03-26 13:56:17 +0000

    utils y eq_download actualizacion

--- utils/statistics.py
+++ utils/statistics.py
@@ -1,13 +1,17 @@
 import statistics as st
 import scipy as scp
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import RectBivariateSpline
+
 
 # Necesito hacer ajustes y sacar todos los datos posibles de estos
 def fit(x_ax, y_ax, order):
     coef = np.polyfit(x_ax, y_ax, i)
     y_pred = np.polyfit(coef, x_ax)
     return coef, y_pred
-    
+
+
 # Comparing the different fit orders and choosing the best one
 def best_fit(x_ax, y_ax, max_order, min_r2):
     best_order = 1
@@ -36,5 +40,27 @@ def best_fit(x_ax, y_ax, max_order, min_r2):
     return np.polyval(x_ax, fit(x_ax, y_ax, best_order)), best_r2 
 
 
+# Bicubic interpolation
 
+def bcb_interp(x_ax, y_ax, z_ax):
+    
+    # Crear interpolador bicúbico
+    interp = RectBivariateSpline(x_ax, y_ax, z_ax)
+
+# Crear una grilla más fina para la interpolación
+    x_fine = np.linspace(0, 10, 50)
+    y_fine = np.linspace(0, 10, 50)
+    X_fine, Y_fine = np.meshgrid(x_fine, y_fine)
+    Z_fine = interp(x_fine, y_fine)
+
+# Graficar
+    plt.figure(figsize=(8,6))
+    plt.pcolormesh(X_fine, Y_fine, Z_fine, shading='auto', cmap='viridis')
+    plt.colorbar(label='Interpolación')
+    plt.scatter(x_ax, y_ax, color='red', label='Puntos originales')
+    plt.legend()    
+    plt.title('Interpolación Bicúbica')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
 
