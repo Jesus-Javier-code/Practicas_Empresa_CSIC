93155e230292c7d9a415d6175bc42407ea9c371f
Jesus-Javier-code <alu0101465142@ull.edu.es>
2025-03-31 09:57:12 +0100

    a

--- 02_utils/geometry.py
+++ 02_utils/geometry.py
@@ -4,12 +4,12 @@ import pandas as pd
 import plotly.graph_objects as go
 import os
 
-# Creating a function to show a grid in function of the zoomlevel
+# Crear una función para mostrar una cuadrícula según el nivel de zoom
 def grid(fig, lat_min, lat_max, lon_min, lon_max):
-    # The number of cells can be changed here by changing the number
+    # El número de celdas puede cambiarse aquí modificando el número
     n_rows = 10
     n_columns = 10
-    # Distance between cells
+    # Distancia entre celdas
     lat_step = (lat_max - lat_min) / n_rows
     lon_step = (lon_max - lon_min) / n_columns
     
@@ -28,8 +28,8 @@ def grid(fig, lat_min, lat_max, lon_min, lon_max):
                 fillcolor="rgba(0, 0, 0, 1)"
             )
 
-# pos1 and pos2 are the positions of the area you would like to see
-def geo_map(pos1, pos2, zone):
+# pos1 y pos2 son las posiciones del área que quieres mostrar
+def geo_map(pos1, pos2, zone, output_file):
     lat_min = min(pos1[0], pos2[0])
     lat_max = max(pos1[0], pos2[0])
     lon_min = min(pos1[1], pos2[1])
@@ -44,7 +44,7 @@ def geo_map(pos1, pos2, zone):
         "description": [zone]
     })
     
-    # Creating the map
+    # Crear el mapa
     fig = go.Figure()
 
     # Configurar el mapa base (puedes usar varios estilos como 'stamen-terrain', 'carto-positron', 'open-street-map', etc.)
@@ -54,7 +54,7 @@ def geo_map(pos1, pos2, zone):
         dragmode=False
     )
 
-    # To show the area delimitation
+    # Mostrar la delimitación del área
     fig.add_trace(go.Scattermap(
         mode="lines",
         lon=[lon_min, lon_max, lon_max, lon_min, lon_min],
@@ -69,7 +69,7 @@ def geo_map(pos1, pos2, zone):
     # Crear la ruta completa para el archivo HTML
     output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../04_web")
     os.makedirs(output_dir, exist_ok=True)  # Crear la carpeta si no existe
-    output_path = os.path.join(output_dir, "mapa_tajogaite.html")
+    output_path = os.path.join(output_dir, output_file)
     
     # Guardar la gráfica interactiva en el archivo HTML
     pio.write_html(fig, output_path, full_html=True)
@@ -83,7 +83,7 @@ pos2_la_palma = np.array([28.62514776637218, -17.872144640744164])
 pos3_la_palma = np.array([28.3, -18.2])
 pos4_la_palma = np.array([28.8, -17.9])
 
-geo_map(pos1_la_palma, pos2_la_palma, "Volcan de Tajogaite")
-geo_map(pos3_la_palma, pos4_la_palma, "Volcan de Tajogaite")
-
+# Llamar a geo_map para ambos mapas
+geo_map(pos1_la_palma, pos2_la_palma, "Volcan de Tajogaite", "mapa_tajogaite.html")
+geo_map(pos3_la_palma, pos4_la_palma, "Región alrededor", "mapa_region_alrededor.html")
 
