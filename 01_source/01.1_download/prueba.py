abbdc45c99773acfe3df15d933376831e97d9712
Jesus-Javier-code <alu0101465142@ull.edu.es>
2025-04-01 08:53:11 +0100

    a

--- 01_source/01.1_download/prueba.py
+++ 01_source/01.1_download/prueba.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
-# Construcción de la ruta relativa desde donde ejecutas el script
-file = Path("../00_data/processed/BT_by_Year_Month_Day/TB_By_Year_Month_Day.nc")
+# Ruta correcta: subir dos niveles para salir de "01_source/01.1_download"
+file = Path("../../00_data/processed/BT_by_Year_Month_Day/TB_By_Year_Month_Day.nc")
 
-# Mostrar información útil
 print("Ruta absoluta que se está usando:", file.resolve())
 print("El archivo existe?", file.exists())
 
